ad177399e95e4b785c34f746edf367323c55bead
x105strike <x105strike.2023@gmail.com>
2025-04-11 16:26:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
+
 @client.event
 async def on_ready():
     print(f"ログイン完了：{client.user}")
